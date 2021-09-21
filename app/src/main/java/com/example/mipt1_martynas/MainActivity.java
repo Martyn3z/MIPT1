c671a604bc5160df7238073901f7bf5eaffc1c00
Martyn3z <37242932+Martyn3z@users.noreply.github.com>
2021-09-21 20:41:24 +0300

    Button now works

--- app/src/main/java/com/example/mipt1_martynas/MainActivity.java
+++ app/src/main/java/com/example/mipt1_martynas/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.mipt1_martynas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.TextForButton);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("What have you done..");
     }
 }
